c56d01fcc50f51b5f35c01fb1c3686730cd15996
phyllocrania <34107739+phyllocrania@users.noreply.github.com>
2018-04-30 12:31:29 -0400

    Update interpret.js

--- interpret.js
+++ interpret.js
@@ -1,7 +1,11 @@
 function understand(arr) {
 words = arr.split(" ")
 for (var i = 0; i < words.length; i++) {
+  if (vocab[words[i]] !== undefined) {
   alert(vocab[words[i]])
+  } else {
+    alert(words[i])
+  }
 }
 
 var vocab = {
